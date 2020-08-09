66767490d06c1b872a07f180e32e3418a12bab24
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-08-08 21:53:37 -0400

    getway added

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -4,10 +4,20 @@ require('dotenv').config();
 
 
 
-exports.generateToken = (req, res) => {
-    
-};
+const gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox, // Production
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY
+});
 
-exports.processPayment = (req, res) => {
-   
-};  
+
+exports.generateToken = (req, res) => {
+    gateway.clientToken.generate({}, function(err, response) { 
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(response);
+        }
+    });
+};
